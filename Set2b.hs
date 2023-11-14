465b37d24509977c7e6f5146ad31496b4df755af
seolbin <jsb3038@gmail.com>
2023-11-15 00:51:06 +0900

    Set2b EX3 commit
    Signed-off-by: seolbin <jsb3038@gmail.com>

--- Set2b.hs
+++ Set2b.hs
@@ -66,7 +66,11 @@ oddFactorial n
 -- * https://en.wikipedia.org/wiki/Euclidean_algorithm
 
 myGcd :: Integer -> Integer -> Integer
-myGcd = todo
+myGcd a b
+  | a == 0    = b
+  | b == 0    = a
+  | a > b     = myGcd (a - b) b
+  | otherwise = myGcd a (b - a)
 
 ------------------------------------------------------------------------------
 -- Ex 4: Implement the function leftpad which adds space characters
