c37bc55f7ea2971fef0328dac13088951bd63618
seolbin <jsb3038@gmail.com>
2023-12-01 20:14:02 +0900

    Set3b EX7
    Signed-off-by: seolbin <jsb3038@gmail.com>

--- Set3b.hs
+++ Set3b.hs
@@ -145,7 +145,11 @@ sumsOfHelper (x:xs) acc = (x + acc) : sumsOfHelper xs (x + acc)
 --   merge [1,1,6] [1,2]   ==> [1,1,1,2,6]
 
 merge :: [Int] -> [Int] -> [Int]
-merge xs ys = todo
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+  | x <= y    = x : merge xs (y:ys)
+  | otherwise = y : merge (x:xs) ys
 
 ------------------------------------------------------------------------------
 -- Ex 8: compute the biggest element, using a comparison function
