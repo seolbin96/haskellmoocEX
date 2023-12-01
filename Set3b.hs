89b6d6e3192516b195089f95fecc7acfd0d19d06
seolbin <jsb3038@gmail.com>
2023-12-01 20:02:52 +0900

    Set3b EX3
    Signed-off-by: seolbin <jsb3038@gmail.com>

--- Set3b.hs
+++ Set3b.hs
@@ -71,7 +71,10 @@ sumsHelper current i acc
 --   mylast 0 [1,2,3] ==> 3
 
 mylast :: a -> [a] -> a
-mylast def xs = todo
+mylast def []     = def
+mylast def [x]    = x
+mylast def (_:xs) = mylast def xs
+
 
 ------------------------------------------------------------------------------
 -- Ex 4: safe list indexing. Define a function indexDefault so that
