c22995294caad480a23f41e8126a06c520bf4296
seolbin <jsb3038@gmail.com>
2023-12-01 19:29:31 +0900

    Set3a EX7
    Signed-off-by: seolbin <jsb3038@gmail.com>

--- Set3a.hs
+++ Set3a.hs
@@ -147,8 +147,10 @@ powers k max = takeWhile (<= max) (map (k^) [0..])
 --   in while check tail "xyzAvvt"
 --     ==> Avvt
 
-while :: (a->Bool) -> (a->a) -> a -> a
-while check update value = todo
+while :: (a -> Bool) -> (a -> a) -> a -> a
+while check update value
+  | check value = while check update (update value)
+  | otherwise    = value
 
 ------------------------------------------------------------------------------
 -- Ex 8: another version of a while loop. This time, the check
